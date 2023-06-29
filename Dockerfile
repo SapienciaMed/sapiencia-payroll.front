e416eb01213cc47be6f4991ab03fdb705ded6aa3
Daniel Flores <dflores@i4digital.com>
2023-06-28 19:29:55 -0500

    init

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:14-alpine
+WORKDIR /app
+COPY package*.json ./
+RUN npm install
+
+COPY . .
+RUN npm run build
+EXPOSE 9005
+CMD ["npm", "run", "start"]
---
+++ src/_app.tsx
@@ -0,0 +1,27 @@
+import { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AppContextProvider } from "./common/contexts/app.context";
+import "./styles/_app.scss";
+import "./styles/_theme-prime.css";
+import "primereact/resources/primereact.min.css";
+import ModalMessageComponent from "./common/components/modal-message.component";
+import ApplicationProvider from "./application-provider";
+
+function App() {
+  return (
+    <AppContextProvider>
+      <ModalMessageComponent />
+      <ApplicationProvider>
+        <Router>
+          <Suspense fallback={<p>Loading...</p>}>
+            <Routes>
+              <Route path={"/*"} element={<div>Home</div>} />
+            </Routes>
+          </Suspense>
+        </Router>
+      </ApplicationProvider>
+    </AppContextProvider>
+  );
+}
+
+export default App;

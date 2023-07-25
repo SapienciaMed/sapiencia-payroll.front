c7db1d73e59cc23a32fd4c85d6b810ddd6604e66
Jesteban97 <jearistizabal@i4digital.com>
2023-07-25 17:44:39 -0500

    Update helpers.ts
    add function calculate by days

--- src/common/utils/helpers.ts
+++ src/common/utils/helpers.ts
@@ -1,8 +1,17 @@
-export function calculateDifference(dateInit: string | Date,dateEnd?:string | Date) {
+export function calculateDifferenceYear(dateInit: string | Date,dateEnd?:string | Date) {
     const currentDate = dateEnd ? new Date(dateEnd) : new Date();
     const differenceInMilliseconds =
       currentDate.getTime() - new Date(dateInit).getTime();
     const differenceInYears =
       differenceInMilliseconds / (24 * 60 * 60 * 1000 * 365);
     return Math.floor(differenceInYears);
+  }
+
+  export function calculateDifferenceDays(dateInit: string | Date,dateEnd?:string | Date) {
+    const currentDate = dateEnd ? new Date(dateEnd) : new Date();
+    const differenceInMilliseconds =
+      currentDate.getTime() - new Date(dateInit).getTime();
+    const differenceInDays =
+      differenceInMilliseconds / (24 * 60 * 60 * 1000);
+    return Math.floor(differenceInDays);
   }

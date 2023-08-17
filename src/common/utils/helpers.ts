6416674d8a3a537becbc3d142de6460a3b17e763
Jesteban97 <jearistizabal@i4digital.com>
2023-08-17 14:09:10 -0500

    ajustes navigate
    ajustes navigate

--- src/common/utils/helpers.ts
+++ src/common/utils/helpers.ts
@@ -80,7 +80,6 @@ export function formaterNumberToCurrency(number) {
 
 
 export function addBusinessDays(startDate, daysToAdd, holidays = []) {
-  debugger
   const oneDay = 24 * 60 * 60 * 1000; // Un día en milisegundos
   let currentDate = new Date(startDate);
 

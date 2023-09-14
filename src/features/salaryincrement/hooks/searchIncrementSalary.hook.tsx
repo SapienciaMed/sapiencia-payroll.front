aa429cd1b41e5adecb2e5343b211c41f95c42c0f
Jesteban97 <jearistizabal@i4digital.com>
2023-09-14 18:24:08 -0500

    Update searchIncrementSalary.hook.tsx

--- src/features/salaryincrement/hooks/searchIncrementSalary.hook.tsx
+++ src/features/salaryincrement/hooks/searchIncrementSalary.hook.tsx
@@ -137,7 +137,7 @@ export default function useSearchIncrementSalaryHook() {
         },
         {
           title: "Fecha efectiva",
-          value: (DateTime.fromISO(row?.effectiveDate).toString()),
+          value: DateTime.fromISO(row?.effectiveDate).toLocaleString(),
         },
       ];
 

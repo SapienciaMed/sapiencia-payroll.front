57afebc0cd6bf16400d4a7dbea1c45ad2d83cef0
Jesteban97 <jearistizabal@i4digital.com>
2023-10-27 13:29:20 -0500

    Ajustes

--- src/common/schemas/employment-schema.ts
+++ src/common/schemas/employment-schema.ts
@@ -236,7 +236,7 @@ export const createDeductionTwoSchema = yup.object({
   codDeductionType: yup.string().required("El campo es obligatorio"),
   codFormsPeriod: yup.string().required("El campo es obligatorio"),
   value: yup.number().required("El campo es obligatorio"),
-  observation: yup.string().max(500, "Solo se permiten 500 caracteres"),
+  //observation: yup.string().max(500, "Solo se permiten 500 caracteres"),
 });
 
 export const createDeductionThreeSchema = yup.object({
@@ -245,7 +245,7 @@ export const createDeductionThreeSchema = yup.object({
   codDeductionType: yup.string().required("El campo es obligatorio"),
   codFormsPeriod: yup.string().required("El campo es obligatorio"),
   value: yup.number().required("El campo es obligatorio"),
-  observation: yup.string().max(500, "Solo se permiten 500 caracteres"),
+  //observation: yup.string().max(500, "Solo se permiten 500 caracteres"),
 });
 
 export const formDeduction = [
